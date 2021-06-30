699db839ab76a8cf851b6a84a7174f5ae8f2c4e2
KXRXH <74421236+KXRXH@users.noreply.github.com>
2021-06-30 18:55:28 +0700

    Image saving was added!
    -choosing boxes color now is available

--- detect.py
+++ detect.py
@@ -1,8 +1,7 @@
-from .utils import *
-
 from detecto.utils import read_image
 from detecto.core import Dataset
 from detecto.core import Model
+from PIL import Image, ImageDraw
 
 
 class Finder:
@@ -15,19 +14,32 @@ class Finder:
         img = read_image(image_file)
         labels, boxes, scores = self.model.predict(img)
         print(labels, boxes, scores, sep='\n')
-        slugs = [make_slug(b, s) for b, s in zip(labels, scores)]
+        draw_and_save(img_file=image_file, boxes=boxes, file_save_name="out_" + image_file)
         bears = [map(round, react.tolist()) for react in boxes]
-        return bears, slugs
+        return bears
 
-    def find_bears(self, image_file: str) -> bytes:
-        bears, slugs = self.find(image_file)
-        return b'' # TODO
+    # def find_bears(self, image_file: str) -> bytes:
+    #    bears, slugs = self.find(image_file)
+    #   return b''  # TODO
 
     def learn(self, path: str, epochs: int, learning_rate: float, lr_step_size: int, verbose: bool):
         MyDataset = Dataset(path)
         model = Model([self.desired_class])
-        model.fit(dataset=MyDataset, epochs=epochs, learning_rate=learning_rate, lr_step_size=lr_step_size, verbose=verbose)
+        model.fit(dataset=MyDataset, epochs=epochs, learning_rate=learning_rate, lr_step_size=lr_step_size,
+                  verbose=verbose)
         model.save(self.model_file)
+
+
+def draw_and_save(img_file: str, boxes: list, color: str = "#1700FF", file_save_name="outFile.JPG"):
+    source_file = Image.open(img_file).convert("RGBA")
+    draw = ImageDraw.Draw(source_file)
+    for i in range(boxes.shape[0]):
+        box = boxes[i]
+        # box[0]: x0, box[1]: y0, box[2]: x1, box[3]: y1
+        draw.rectangle([box[0].item(), box[1].item(), box[2].item(), box[3].item()], outline=color, fil=None,
+                       width=2)
+    source_file.save(file_save_name)
+
 
 def run():
     Finder("MyModel.pth", "WhiteBear")
