1ec6e4bdc9e5f21295ccda9401da71006c359464
BigCubeCat <>
2021-06-30 19:27:52 +0700

    fix errors

--- detect.py
+++ detect.py
@@ -1,7 +1,7 @@
+from utils import draw_boxes
 from detecto.utils import read_image
 from detecto.core import Dataset
 from detecto.core import Model
-from PIL import Image, ImageDraw
 
 
 class Finder:
@@ -12,15 +12,8 @@ class Finder:
 
     def find(self, image_file: str) -> tuple:
         img = read_image(image_file)
-        labels, boxes, scores = self.model.predict(img)
-        print(labels, boxes, scores, sep='\n')
-        draw_and_save(img_file=image_file, boxes=boxes, file_save_name="out_" + image_file)
-        bears = [map(round, react.tolist()) for react in boxes]
-        return bears
-
-    # def find_bears(self, image_file: str) -> bytes:
-    #    bears, slugs = self.find(image_file)
-    #   return b''  # TODO
+        _, boxes, _ = self.model.predict(img)
+        return draw_boxes(img_file=image_file, boxes=boxes) # file_save_name="out_" + image_file)
 
     def learn(self, path: str, epochs: int, learning_rate: float, lr_step_size: int, verbose: bool):
         MyDataset = Dataset(path)
@@ -29,17 +22,10 @@ class Finder:
                   verbose=verbose)
         model.save(self.model_file)
 
-
-def draw_and_save(img_file: str, boxes: list, color: str = "#1700FF", file_save_name="outFile.JPG"):
-    source_file = Image.open(img_file).convert("RGBA")
-    draw = ImageDraw.Draw(source_file)
-    for i in range(boxes.shape[0]):
-        box = boxes[i]
-        # box[0]: x0, box[1]: y0, box[2]: x1, box[3]: y1
-        draw.rectangle([box[0].item(), box[1].item(), box[2].item(), box[3].item()], outline=color, fil=None,
-                       width=2)
-    source_file.save(file_save_name)
+def main():
+    f = Finder("MyModel.pth", "WhiteBear")
+    f.find(input("filename=")).show()
 
 
-def run():
-    Finder("MyModel.pth", "WhiteBear")
+if __name__ =='__main__':
+    main()
--- utils.py
+++ utils.py
@@ -1,3 +1,14 @@
-def make_slug(class_name: str, probability: float) -> str:
-    persents = round(probability)
-    return f"{class_name} {persents}"
+from PIL import Image, ImageDraw
+
+
+def draw_boxes(img_file: str, boxes: list, color: str="#1700FF") -> Image: # file_save_name="outFile.JPG") -> Image:
+    source_file = Image.open(img_file).convert("RGB")
+    draw = ImageDraw.Draw(source_file)
+    for i in range(boxes.shape[0]):
+        box = boxes[i]
+        # box[0]: x0, box[1]: y0, box[2]: x1, box[3]: y1
+        draw.rectangle([box[0].item(), box[1].item(), box[2].item(), box[3].item()], outline=color, fill=None,
+                       width=2)
+    # source_file.save(file_save_name)
+    return source_file
+
