f46ffc0b22f3ff48608c24a75d882c620350d7b9
BigCubeCat <>
2021-07-01 23:00:44 +0700

    v1

--- __init__.py
+++ __init__.py
@@ -10,10 +10,10 @@ def init(model_path):
     finder = Finder(model_path)
 
 
-def image_file(input_file: str, output_file: str):
-    image = finder.find(image_file)
+def image_file(input_path: str, output_file: str):
+    image = finder.find(input_path)
     image.save(output_file)
 
 
-def image_pillow(image_file: str, color: str) -> Image:
-    return finder.find(image_file, color=color)
+def image_pillow(image_path: str, color: str) -> Image:
+    return finder.find(image_path, color=color)
